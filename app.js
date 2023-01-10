32f3627b953ac864de0c85d8b11b4ed3141bab04
Georgy Kcojev <kcojev@gmail.com>
2023-01-10 17:32:51 +0300

    js file done

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const toggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+
+toggle.addEventListener('click', () => {
+    links.classList.toggle('show-links')
+})
